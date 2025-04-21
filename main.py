4460e4bd2441eda821ad31e53e65ce99db4ddeea
icashero95 <iancasheroweb34@gmail.com>
2025-04-20 23:36:54 -0400

    Added an fps limiter, and ade the close button work

--- main.py
+++ main.py
@@ -5,17 +5,22 @@ import pygame
 from constants import *
 
 def main():
-    pygame.init
+    pygame.init()
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0, 0, 0), special_flags=0)
         pygame.display.flip()
+        time_passed = clock.tick(60)
+        dt = time_passed / 1000
 
 if __name__ == "__main__":
     main()

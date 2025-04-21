a85447fd7d69fb41fb48160f189731c48e32ae95
icashero95 <iancasheroweb34@gmail.com>
2025-04-21 00:49:38 -0400

    Made the ship rotate

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ def main():
                 return
         screen.fill((0, 0, 0))
         ship.draw(screen)
+        ship.update(dt)
         pygame.display.flip()
         time_passed = clock.tick(60)
         dt = time_passed / 1000

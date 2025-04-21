fa00a88b79237d08a898869b63bcc26843791f49
icashero95 <iancasheroweb34@gmail.com>
2025-04-20 23:02:23 -0400

    Fixed an error in the terminal

--- main.py
+++ main.py
@@ -5,9 +5,14 @@ import pygame
 from constants import *
 
 def main():
+    pygame.init
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        pygame.display.fill("black", rect=screen, special_flags=0)
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
